90063591a89645905d91da2a38a16d0abe19df39
Cara Sunberg <cnsunberg@gmail.com>
2019-11-11 09:38:40 -0500

    Added functions so that the game starts: -Only goes through the first round -Player can't click buttons yet

--- index.js
+++ index.js
@@ -29,3 +29,109 @@ const bottomRight = document.querySelector("#bottomright");
 const strictButton = document.querySelector("#strict");
 const onButton = document.querySelector("#on");
 const startButton = document.querySelector("#start");
+
+strictButton.addEventListener("change", event => {
+    if (strictButton.checked) {
+        strict = true;
+    } else {
+        strict = false;
+    }
+});
+
+onButton.addEventListener("click", event => {
+    if (onButton.checked) {
+        on = true;
+        turnCounter.innerHTML = "-";
+    } else {
+        on = false;
+        turnCounter.innerHTML = "";
+        clearColor();
+        clearInterval(intervalId);
+    }
+});
+
+startButton.addEventListener("click", event => {
+    if (on || win) {
+        play();
+    }
+});
+
+function play() {
+    win = false;
+    order = [];
+    playerOrder = [];
+    flash = 0;
+    intervalId = 0;
+    turn = 1;
+    turnCounter.innerHTML = 1;
+    good = true;
+    for (let i = 0; i < 20; i++) {
+        order.push(Math.floor(Math.random() * 4) + 1);
+    }
+    compTurn = true;
+
+    intervalId = setInterval(gameTurn, 800);
+};
+
+function gameTurn() {
+    on = false;
+    if (flash === turn) {
+        clearInterval(intervalId);
+        compTurn = false;
+        clearColor();
+        on = true;
+    }
+    if (compTurn) {
+        clearColor();
+        setTimeout(() => {
+            if (order[flash] === 1) one();
+            if (order[flash] === 2) two();
+            if (order[flash] === 3) three();
+            if (order[flash] === 4) four();
+            flash++;
+        }, 200)
+    }
+};
+
+function one() {
+    if (noise) {
+        const audio = document.getElementById("clip1");
+        audio.play();
+    }
+    noise = true;
+    topLeft.style.backgroundColor = "lightgreen";
+};
+
+function two() {
+    if (noise) {
+        const audio = document.getElementById("clip2");
+        audio.play();
+    }
+    noise = true;
+    topRight.style.backgroundColor = "tomato";
+};
+
+function three() {
+    if (noise) {
+        const audio = document.getElementById("clip3");
+        audio.play();
+    }
+    noise = true;
+    bottomLeft.style.backgroundColor = "yellow";
+};
+
+function four() {
+    if (noise) {
+        const audio = document.getElementById("clip4");
+        audio.play();
+    }
+    noise = true;
+    bottomRight.style.backgroundColor = "lightskyblue";
+};
+
+function clearColor() {
+    topLeft.style.backgroundColor = "darkgreen";
+    topRight.style.backgroundColor = "darkred";
+    bottomLeft.style.backgroundColor = "goldenrod";
+    bottomRight.style.backgroundColor = "darkblue";
+}
